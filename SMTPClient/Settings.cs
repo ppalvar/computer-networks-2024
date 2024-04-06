e3bba20291aef9b712e260222eec11479dffe7e9
Pedro P. Alvarez Portelles <ppalvarez2002@gmail.com>
2024-04-06 14:51:53 -0400

    Add settings file

---
+++ SMTPClient/Settings.cs
@@ -0,0 +1,7 @@
+namespace EmailClient.Settings;
+
+public static class Config
+{
+    public static readonly bool isDevelopment = false;
+    public static readonly bool trustSslCertificates = true;
+}
